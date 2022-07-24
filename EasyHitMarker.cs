1f5be6f3c9deaf14e412158ecbf67b5c297b4440
uwu-ohayo <57017651+uwu-ohayo@users.noreply.github.com>
2022-07-24 21:20:08 +0700

    hitmarker update

--- EasyHitMarker.cs
+++ EasyHitMarker.cs
@@ -9,19 +9,20 @@ namespace HitMarker
     {
         public override string Developer => "ohayo!#5601";
         public override string Name => "EasyHitMarker";
-        public override Version Version => new Version(1, 0, 1);
+        public override Version Version => new Version(1, 0, 2);
         public override int Priority => int.MinValue;
-
-        public static bool Enabled { get; internal set; }
 
         public override void Enable()
         {
-            String PluginName = Config.GetString("hitmarker_plugin_name", "EasyHitMarker");
-            String custmess = Config.GetString("hitmarker_hint_message", "-<color=#F51FDE>{damageamount}</color> HP");
-            bool Enabled = Config.GetBool("hitmarker_enable", true);
-            float hintduration = Config.GetFloat("hitmarker_hint_duration", 1);
+            String hitmarker_plugin_name = Config.GetString("hitmarker_plugin_name", "EasyHitMarker");
+            bool hitmarker_enable = Config.GetBool("hitmarker_enable", true);
+            String hitmarker_hint_message = Config.GetString("hitmarker_hint_message", "-<color=#F51FDE>{damageamount}</color> HP");
+            float hitmarker_hint_duration = Config.GetFloat("hitmarker_hint_duration", 1);
+            int hitmarker_randShow1 = Config.GetInt("hitmarker_randShow1", -50);
+            int hitmarker_randShow2 = Config.GetInt("hitmarker_randShow2", 50);
 
-            if (!Enabled) {
+
+            if (hitmarker_enable == false) {
                 Log.Error(" the hitmarker is disabled because you disabled it in the config");
                 return;
             }
@@ -29,7 +30,7 @@ namespace HitMarker
             Player.DamageProcess += OnDamageProcess;
 
             Log.Info(" hitmarker enabled!!! :)");
-            Log.Info(" version: 1.0.1");
+            Log.Info(" version: 1.0.2");
             Log.Info(" dev: ohayo!#5601");
         }
 
@@ -38,30 +39,26 @@ namespace HitMarker
             Player.DamageProcess -= OnDamageProcess;
 
             Log.Info(" hitmarker disabled!!! :(");
-            Log.Info(" version: 1.0.1");
+            Log.Info(" version: 1.0.2");
             Log.Info(" dev: ohayo!#5601");
         }
 
         public static void OnDamageProcess(DamageProcessEvent ev)
         {
             Random r = new Random();
-
-            String ns = "";
-
             int damageamount = (int)ev.Amount;
 
             float durationhint = Config.GetFloat("hitmarker_hint_duration", 1);
-
+            int rand1 = Config.GetInt("hitmarker_randShow1", -50);
+            int rand2 = Config.GetInt("hitmarker_randShow2", 50);
             String message = Config.GetString("hitmarker_hint_message", "-<color=#F51FDE>{damageamount}</color> HP").Replace("{damageamount}", $"{damageamount}");
-
-            for (int i = 0; i < r.Next(4, 12); i++)
-            {
-                ns += "\n";
-            }
 
             if (ev.Attacker.Team != Team.SCP)
             {
-                ev.Attacker.ShowHint($"<pos={r.Next(-15, 30)}%>{message}{ns}", durationhint);
+                if (ev.Attacker.Team != ev.Target.Team)
+                {
+                    ev.Attacker.ShowHint($"<pos={r.Next(rand1, rand2)}%>{message}", durationhint);
+                }
             }
         }
             
