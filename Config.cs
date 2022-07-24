a6f66c7e62459b37a357ad509b1c52a06e243cd9
uwu-ohayo <57017651+uwu-ohayo@users.noreply.github.com>
2022-07-24 20:25:54 +0700

    new config

--- Config.cs
+++ Config.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel;
+using System.ComponentModel;
 
 namespace EasyHitMarker
 {
@@ -12,5 +12,9 @@ namespace EasyHitMarker
         public string hitmarker_hint_message { get; set; } = "-<color=#F51FDE>{damageamount}</color> HP";
         [Description("duration for 1 hint")]
         public float hitmarker_hint_duration { get; set; } = 1;
+        [Description("rand1 value")]
+        public int hitmarker_randShow1 { get; set; } = -50;
+        [Description("rand2 value")]
+        public int hitmarker_randShow2 { get; set; } = 50;
     }
 }
